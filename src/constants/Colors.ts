6a3b4ca3bc8de3fbb9a984f8fea90ba575b9a2d0
G-nexyield <guilherme@nexyield.com>
2023-12-26 21:06:14 +0000

    feat:  add Colors constants

---
+++ src/constants/Colors.ts
@@ -0,0 +1,11 @@
+export default {
+  blue: "#275EF6",
+  pageHeaders: "#0B295A",
+  bodyText: "#000000",
+  textInputBackground: "#F5F5F5",
+  textInputPlaceholder: "#999999",
+  textInputColor: "#000000",
+  greyButtonBackground: "#B8B8B8",
+  greyButtonText: "#4D4D4D",
+  red: "#FF0000",
+};
